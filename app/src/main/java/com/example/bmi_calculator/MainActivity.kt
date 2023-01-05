ad8276e2108e8aa53ab8c43e1c383d531f9278ad
MORGUE28 <krubhere123@gmail.com>
2023-01-05 15:59:10 +0530

    added alert dialogue box

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -14,20 +14,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         calcBMI.setOnClickListener {
-
+            lateinit var output:String
+            lateinit var output2: String
             val weight = wgt.text.toString().toFloat()
             val height = hgt.text.toString().toFloat()
             val BMI = ((weight)/(height*height))
-            output.text = BMI.toString()
+            output = BMI.toString()
 
             if (BMI < 18.5) {
-                output2.text = "You are Under-weight"
+                output2 = "You are Under-weight"
             }
             else if (BMI > 18.5 && BMI < 25){
-                output2.text = "You are Normal"
+                output2 = "You are Normal"
             }else{
-                output2.text = "You are Over-weight"
+                output2 = "You are Over-weight"
+            }
+
+            val dialogClickListener =
+                DialogInterface.OnClickListener { dialog, which ->
+                    when (which) {
+                        DialogInterface.BUTTON_NEGATIVE -> {
+                            dialog.dismiss()
+                        }
+                    }
+                }
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+
+            builder.setMessage("BMI INDEX:$output \n$output2")
+                .setTitle("BMI")
+                .setNegativeButton("OK", dialogClickListener)
+                .show()
             }
         }
     }
-}
+
